83e51b19455f5c17105f6062e54f7ecc05469f86
Bekirsengul <bekirsengul06@gmail.com>
2021-06-22 12:37:49 +0200

    added works

--- src/work1/Helloclass.java
+++ src/work1/Helloclass.java
@@ -5,6 +5,6 @@ public class Helloclass {
     public static void main(String[] args) {
         System.out.println("Work1 started");
         System.out.println("Created Git repository");
-        System.out.println("Made few commits");
+        System.out.println("Made few commit");
     }
 }
